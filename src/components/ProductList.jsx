fd768a138bb820fcce1ae3c055e93f59024d0e4e
Cecilia Blankson Amponsah <amponsahcecilia77@gmail.com>
2022-08-22 13:58:24 +0000

    custom-hook

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Product from "../Product";
+import useAxios from "../hooks/useAxios";
+import Spinner from "./Spinner";
 
 function ProductList() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    const getProducts = async () => {
-      const results = await axios.get(
-        "https://api.escuelajs.co/api/v1/products"
-      );
-      setProducts(results.data);
-    };
-    getProducts();
-  });
+  //   const {data, isLoading, error} = useAxios()
+  const { data, isLoading, error } = useAxios(
+    "https://api.escuelajs.co/api/v1/products"
+  );
+  if (isLoading) return <Spinner />;
+  if (error) return <p>{error}</p>;
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const results = await axios.get(
+  //       "https://api.escuelajs.co/api/v1/products"
+  //     );
+  //     setProducts(results.data);
+  //   };
+  //   getProducts();
+  // });
 
   return (
     <div className="flex flex-wrap gap-10 justify-center pt-10">
-      {products.map((product) => (
-        <Product product={product} />
+      {data.map((data) => (
+        <Product product={data} />
       ))}
     </div>
   );
