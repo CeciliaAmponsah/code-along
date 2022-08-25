f1f7303946fb6d26ae742ccc4cdb819d4703e61d
Cecilia Blankson Amponsah <amponsahcecilia77@gmail.com>
2022-08-25 14:15:37 +0000

    State-management-1

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,12 +3,19 @@ import axios from "axios";
 import Product from "../components/Product";
 import useAxios from "../hooks/useAxios";
 import Spinner from "../components/Spinner";
+import { useProductContext } from "../context/productContext";
 
 function ProductList() {
   //   const {data, isLoading, error} = useAxios()
   const { data, isLoading, error } = useAxios(
-    "https://api.escuelajs.co/api/v1/products"
+    "https://fakestoreapi.com/products"
   );
+  const { products, setProducts } = useProductContext();
+
+  useEffect(() => {
+    setProducts(data);
+  }, [data]);
+
   if (isLoading) return <Spinner />;
   if (error) return <p>{error}</p>;
 
@@ -22,9 +29,11 @@ function ProductList() {
   //   getProducts();
   // });
 
+  console.log(products);
+
   return (
     <div className="flex flex-wrap gap-10 justify-center pt-10">
-      {data.map((data) => (
+      {products?.map((data) => (
         <Product product={data} />
       ))}
     </div>
